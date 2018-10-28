2111f0e1726409385a5b9679c81483583175448c
Alan Vardy <alanvardy@gmail.com>
2018-10-28 10:20:09 -0600

    new matrix version

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,20 +1,15 @@
 class Matrix
   def initialize(sequence)
-    @lines = sequence.split("\n")
-    @lines.each_index do |i|
-      @lines[i] = @lines[i].scan(/\d+/).map(&:to_i)
-    end
+    @sequence = sequence
   end
 
   def rows
-    @lines
+    @sequence.split("\n").collect do |line|
+      line.scan(/\d+/).map!(&:to_i)
+    end
   end
 
   def columns
-    columns = []
-    @lines.each_index do |i|
-      columns << @lines.map { |x| x[i] }
-    end
-    columns
+    rows.transpose
   end
 end
