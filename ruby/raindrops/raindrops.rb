6de642b4a886d3685f3124a8f6ec70ebc15322fe
Alan Vardy <alanvardy@gmail.com>
2018-11-12 08:56:29 -0700

    wip

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,9 +1,15 @@
+# Convert integer to a string based on divisors
 module Raindrops
+  SOUNDS = {
+    'Pling' => 3,
+    'Plang' => 5,
+    'Plong' => 7
+  }.freeze
+
   def self.convert(number)
-    answer = ''
-    answer += 'Pling' if number % 3 == 0
-    answer += 'Plang' if number % 5 == 0
-    answer += 'Plong' if number % 7 == 0
+    answer = SOUNDS.map do |sound, divisor|
+      sound if (number % divisor).zero?
+    end.join
     answer.empty? ? number.to_s : answer
   end
 end
