40c673e386a2db39fc1e31a99408fb4b72782e20
Alan Vardy <alanvardy@gmail.com>
2019-02-24 10:22:57 -0700

    wip

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,7 +1,29 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      true -> raise "Your implementation goes here"
+      Regex.match?(~r{[A-Z]!}, input) ->
+        "Whoa, chill out!"
+
+      Regex.match?(~r{[A-Z]\?}, input) ->
+        "Calm down, I know what I'm doing!"
+
+      Regex.match?(~r{\?$}, input) ->
+        "Sure."
+
+      Regex.match?(~r{\?}, input) ->
+        "Whatever."
+
+      Regex.match?(~r/[[:upper:]]{2,}/u, input) ->
+        "Whoa, chill out!"
+
+      Regex.match?(~r{!}, input) ->
+        "Whatever."
+
+      !Regex.match?(~r{\w}, input) ->
+        "Fine. Be that way!"
+
+      true ->
+        "Whatever."
     end
   end
 end
