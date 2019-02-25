529fe10fc2d392076d8bdb25de0b3b862b06dbbb
Alan Vardy <alanvardy@gmail.com>
2019-02-25 15:58:48 -0700

    Use cond

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,29 +1,14 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.match?(input, ~r{[A-Z]!}) ->
-        "Whoa, chill out!"
-
-      String.match?(input, ~r{[A-Z]\?}) ->
-        "Calm down, I know what I'm doing!"
-
-      String.match?(input, ~r{\?$}) ->
-        "Sure."
-
-      String.match?(input, ~r{\?}) ->
-        "Whatever."
-
-      String.match?(input, ~r/[[:upper:]]{2,}/u) ->
-        "Whoa, chill out!"
-
-      String.match?(input, ~r{!}) ->
-        "Whatever."
-
-      !String.match?(input, ~r{\w}) ->
-        "Fine. Be that way!"
-
-      true ->
-        "Whatever."
+      input =~ ~r{[A-Z]!} -> "Whoa, chill out!"
+      input =~ ~r{[A-Z]\?} -> "Calm down, I know what I'm doing!"
+      input =~ ~r{\?$} -> "Sure."
+      input =~ ~r{\?} -> "Whatever."
+      input =~ ~r/[[:upper:]]{2,}/u -> "Whoa, chill out!"
+      input =~ ~r{!} -> "Whatever."
+      !(input =~ ~r{\w}) -> "Fine. Be that way!"
+      true -> "Whatever."
     end
   end
 end
