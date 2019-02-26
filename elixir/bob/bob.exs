cd7c3178eb765f7107518cce8fb63b0ea76811c3
Alan Vardy <alanvardy@gmail.com>
2019-02-25 20:28:24 -0700

    wip

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -3,10 +3,8 @@ defmodule Bob do
     cond do
       input =~ ~r{[A-Z]!} -> "Whoa, chill out!"
       input =~ ~r{[A-Z]\?} -> "Calm down, I know what I'm doing!"
-      input =~ ~r{\?$} -> "Sure."
-      input =~ ~r{\?} -> "Whatever."
+      String.ends_with?(input, "?") -> "Sure."
       input =~ ~r/[[:upper:]]{2,}/u -> "Whoa, chill out!"
-      input =~ ~r{!} -> "Whatever."
       !(input =~ ~r{\w}) -> "Fine. Be that way!"
       true -> "Whatever."
     end
